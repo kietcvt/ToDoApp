ebb91799f24edc05fd4c598e885929b2e7dfef6b
BMamba2005 <cvotuankiet@gmail.com>
2025-11-10 16:35:28 +0700

    Thêm Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const Footer = () => {
+const Footer = ({ completedTaskCount = 0, activeTaskCount = 0 }) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      {completedTaskCount + activeTaskCount > 0 && (
+        <div className="text-center">
+          <p className="text-sm text-muted-foreground">
+            {completedTaskCount > 0 && (
+              <>
+                Tuyệt vời, bạn đã hoàn thành {completedTaskCount} việc
+                {activeTaskCount > 0 &&
+                  `còn ${activeTaskCount} việc nữa thôi, cố lên cu`}
+              </>
+            )}
+            {completedTaskCount === 0 && activeTaskCount > 0 && (
+              <>Còn tận {activeTaskCount} nhiệm vụ đấy. đi làm ngay đi</>
+            )}
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
